897cc7af02f9f953b082966898c3518c469da9bd
Derr <danr428@gmail.rom>
2018-04-17 21:34:59 +0300

    the key is open

--- src/CryptoMain.java
+++ src/CryptoMain.java
@@ -1,10 +1,9 @@
-import sun.misc.HexDumpEncoder;
-
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import javax.crypto.*;
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+
+
 
 public class CryptoMain {
     public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
@@ -15,28 +14,26 @@ public class CryptoMain {
         KeyGenerator kgen = KeyGenerator.getInstance("AES");
         kgen.init(256);
         SecretKey key = kgen.generateKey();
-        String openkey=key.getEncoded().toString();
-//        char[] openkey  = key.getEncoded();
-        //byte[] openKey =key.getEncoded();
-        System.out.println(openkey);
+
+        //--------I DID IT------------------------------------//
+        String openKey = HexBin.encode(key.getEncoded());
+        System.out.println(openKey);
+        System.out.println("\n");
+        //----------------------------------------------------//
 
         cipher.init(Cipher.ENCRYPT_MODE, key);
-
         byte[] bytes = cipher.doFinal(s.getBytes());
         for (byte b : bytes) {
             System.out.print(b);
         }
-
-
-
 
         System.out.println("\n");
 
         Cipher decryptCipher = Cipher.getInstance("AES");
         decryptCipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decryptedBytes = decryptCipher.doFinal(bytes);
-        for(byte b: decryptedBytes){
+        for (byte b : decryptedBytes) {
             System.out.print((char) b);
         }
+        }
     }
-}
